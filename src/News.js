00ad33d1a231547a6fe46defa2a3d97bfa21aec9
Kasra Sammak <kasra.sammak@mail.mcgill.ca>
2020-11-13 01:31:25 +0100

    added secondless banner to news page

--- src/News.js
+++ src/News.js
@@ -10,11 +10,16 @@ function News(){
     }, []); 
 
   const flyer = "https://lh3.googleusercontent.com/65JfYNHWuito7Y23CkqcAUZnrZSReplUel0GKDlrmxo3PpP2MH_lMTnLD5dEeePzfSN1Nle4fYJHJVuhULYXxl349UHQZ9XFaJSVoA0rphtXDRt5r_FsJVf4VHuSSYBKtt9bCEJ3ig=w2400"
-  
+  const flyer2 = "https://lh3.googleusercontent.com/Zwns8rWFCOp7ZCWc2-Fdbki4ffrfoi1CNYbCQWdW9Nb3aMLEdK9lkXEjUjR1KcOjI80Udn32qF-6rZebQV1HoG_ib1QLmspI_ZEOFeDQfk0C7_pZMTuFytx2oy_E00cbC2HoMMsCXw=w2400"
   return(
     <div className="centerDoc">
-      <h1 className="headers">News</h1>    
-      <img data-aos="fade-up" id="news" src={ flyer } alt="flyer_image"></img>  
+      <h1 className="headers">News</h1>  
+      <div>
+        <img data-aos="fade-up" id="news" src={ flyer2 } alt="flyer_image"></img>   
+      </div>
+      <div>
+        <img data-aos="fade-up" id="news" src={ flyer } alt="flyer_image"></img>   
+      </div>
     </div>         
   )
 }
