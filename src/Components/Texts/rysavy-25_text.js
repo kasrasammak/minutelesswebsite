3ca7dd7a89930d3c54479adbb822a7925a260bcf
Kasra Sammak <kasra.sammak@mail.mcgill.ca>
2021-01-16 23:41:30 +0100

    trying to add og:image

--- src/Components/Texts/rysavy-25_text.js
+++ src/Components/Texts/rysavy-25_text.js
@@ -8,6 +8,7 @@ const Rys25 = () => {
             <p class="description">
                 Third release from Rysavy:  <br></br>
                 Released on January 16, 2021 <br></br><br></br>
+                Special gift for his 25th birthday.<br></br>
                 25 brilliant tracks for 25 brilliant ears or more.
             </p>
         </div>
